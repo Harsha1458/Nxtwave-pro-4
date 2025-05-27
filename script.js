01614b3ff5960c0e09d3c81e9ec6fb29b7fb52f6
Harsha1458 <162542663+Harsha1458@users.noreply.github.com>
2025-05-27 12:02:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,45 @@
-// Select all the span elements inside the h1 with class 'bouncing-letters'
-document.querySelectorAll(".bouncing-letters > span")
-    .forEach(function(element) {
-        // Add mouseover event to each letter
-        element.addEventListener("mouseover", function(e) {
-            bounce(e.target);
-        });
-    });
 
-// Function to add bounce animation class and remove it after 1 second
-function bounce(letter) {
-    if (!letter.classList.contains("bounce")) {
-        letter.classList.add("bounce");
-        setTimeout(function() {
-            letter.classList.remove("bounce");
-        }, 1000);
+body {
+    background-color: rgba(175, 224, 175, 255);
+}
+
+h1 {
+    font-family: sans-serif;
+    font-size: 110px;
+    text-align: center;
+    color: #318D45;
+    margin-top: 250px;
+}
+
+.bouncing-letters span {
+    display: inline-block;
+    animation-timing-function: linear;
+    animation-duration: 1s;
+    animation-iteration-count: 1;
+    cursor: default;
+}
+
+.bouncing-letters span:hover {
+    color: whitesmoke;
+}
+.bounce {
+    animation-name: bounce;
+    animation-duration: 1s;
+    animation-timing-function: ease;
+    animation-iteration-count: 1;
+}
+
+@keyframes bounce {
+    20%{
+        transform: scale(1, 1);
+    }
+    40% {
+        transform: scale(1.75, 0.65);
+    }
+    70% {
+        transform: scale(1.25, 0.75);
+    }
+    90% {
+        transform: scale(1.15, 0.85);
     }
 }
