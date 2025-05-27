4b8f89ab980edfaa68d9e011bde47c899e1cbd3a
Harsha1458 <162542663+Harsha1458@users.noreply.github.com>
2025-05-27 12:03:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,45 +1,15 @@
-
-body {
-    background-color: rgba(175, 224, 175, 255);
+let bouncingLetters = document.getElementsByClassName('bouncing-letters');
+let letters = bouncingLetters[0].getElementsByTagName('span');
+for (let i = 0; i < letters.length; i++) {
+    letters[i].addEventListener("mouseover", function(event) {
+        bounce(event.target);
+    });
 }
-
-h1 {
-    font-family: sans-serif;
-    font-size: 110px;
-    text-align: center;
-    color: #318D45;
-    margin-top: 250px;
-}
-
-.bouncing-letters span {
-    display: inline-block;
-    animation-timing-function: linear;
-    animation-duration: 1s;
-    animation-iteration-count: 1;
-    cursor: default;
-}
-
-.bouncing-letters span:hover {
-    color: whitesmoke;
-}
-.bounce {
-    animation-name: bounce;
-    animation-duration: 1s;
-    animation-timing-function: ease;
-    animation-iteration-count: 1;
-}
-
-@keyframes bounce {
-    20%{
-        transform: scale(1, 1);
-    }
-    40% {
-        transform: scale(1.75, 0.65);
-    }
-    70% {
-        transform: scale(1.25, 0.75);
-    }
-    90% {
-        transform: scale(1.15, 0.85);
+function bounce(letter) {
+    if (!letter.classList.contains("bounce")) {
+        letter.classList.add("bounce");
+        setTimeout(function() {
+            letter.classList.remove("bounce");
+        }, 1000);
     }
 }
